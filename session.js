af30a4f652ca85fd9d2e12e03e55ac14d249b275
Sweezy88 <93490917+Sweezy88@users.noreply.github.com>
2022-01-30 00:34:15 +0200

    Create session.js

---
+++ session.js
@@ -0,0 +1,11 @@
+{
+
+	"clientID": "wYJc0XVdrG/4y6Dek/m4IQ==",	"serverToken": "1@8KefZfw+/TwzObFU7z7DUJa3LmLQuwJwdqztb8HIjhl4NCgOtuXIn9rOTlCBdNOkW/2l/caSPrsr6A==",
+
+	"clientToken": "GVwsVDvkipo0eqIdZMFevPjpcZL5QFfkkn0felQhOyU=",
+
+	"encKey": "4rN9vpYy8TysZdKUvcx+iQ/2SI58K6+NvnblS2p8ij0=",
+
+	"macKey": "G4RCpi6LfdAUs2xdUgCiZUqbU7oSfj1aWL3XwPOdx0E="
+
+}
